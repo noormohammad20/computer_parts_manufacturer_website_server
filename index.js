123e732eb10c72e5aa7a173a7ebe95e92f594429
noormohammad20 <mdsaim204@gmail.com>
2022-05-23 02:42:03 +0600

    create get api for single product load by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const port = process.env.PORT || 5000
 
 app.use(cors())
@@ -17,7 +17,20 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect()
-        console.log('database connected')
+        const productCollection = client.db('computer_parts_manufacturer').collection('products')
+
+        app.get('/product', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await productCollection.findOne(query)
+            res.send(service)
+        })
     }
     finally {
 
